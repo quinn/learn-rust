d2742c5861e8823a4ad85c9478fde44a4c34796a
Quinn Shanahan <q.shanahan@gmail.com>
2020-02-22 20:11:59 -0500

    finished chapter 3

---
+++ chapter-3/functions/src/main.rs
@@ -0,0 +1,19 @@
+// fn main() {
+//     println!("Hello, world!");
+
+//     another_function();
+// }
+
+// fn another_function() {
+//     println!("Another function.");
+// }
+
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ chapter-3/scratch1/src/main.rs
@@ -0,0 +1,37 @@
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     let x = 5;
+
+//     let x = x + 1;
+
+//     let x = x * 2;
+
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     let x: (i32, f64, u8) = (500, 6.4, 1);
+
+//     let five_hundred = x.0;
+
+//     let six_point_four = x.1;
+
+//     let one = x.2;
+
+//     let wrong = x.3;
+// }
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", element);
+}
